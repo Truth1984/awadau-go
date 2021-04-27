bf3af488c1b826d023c5e5556c85c744fb27cf6f
truth1984 <zhengyao365@163.com>
2021-04-27 17:23:44 +0800

    simple update

--- src/main.go
+++ src/main.go
@@ -2,7 +2,33 @@ package main
 
 import u "github.com/Truth1984/awadau-go"
 
+type Foo struct {
+	Bar []string
+	Baz int
+}
+
 func main() {
 	s := [5]int{1, 2, 3, 4, 5}
 	u.Print(u.Types(s))
+
+	v := map[string]int{
+		"d": 32,
+		"r": 55,
+	}
+
+	v2 := map[string]interface{}{
+		"d": 32,
+		"s": "str",
+	}
+
+	u.Print(v)
+
+	u.Print(u.TypesCheck(s, "string"))
+
+	u.Print(u.Types(v))
+
+	u.Print(v2)
+
+	u.Print(u.Types(v2["s"]))
+
 }
--- u.go
+++ u.go
@@ -3,6 +3,7 @@ package u
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 func Print(args ...interface{}) {
@@ -18,9 +19,37 @@ func Types(source interface{}) string {
 
 // @param { "num" } expect
 func TypesCheck(source interface{}, expect string) bool {
+	stype := Types(source)
+
+	switch strings.ToLower(expect) {
+	case "str":
+	case "string":
+		return stype == "string"
+	case "num":
+	case "number":
+		return strings.Contains(stype, "int") || strings.Contains(stype, "float")
+	case "int":
+		return stype == "int"
+	case "float":
+		return strings.Contains(stype, "float")
+	case "arr":
+	case "array":
+		return stype == "array"
+	case "map":
+	case "dict":
+		return stype == "map"
+	default:
+		return strings.Contains(stype, expect)
+	}
 	return false
 }
 
 func Contains(source interface{}, target interface{}) bool {
 	return false
+}
+
+func Ps(structPtr *struct{}) func() {
+	return func() {
+		fmt.Println(reflect.TypeOf(structPtr).String())
+	}
 }
